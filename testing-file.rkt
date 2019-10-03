b0165d6219745667914a2be2b72b0f2947f4e828
morazanm <morazanm@gmail.com>
2019-10-03 13:41:50 -0400

    Visualization Added & clean-up

--- testing-file.rkt
+++ testing-file.rkt
@@ -1,11 +1,30 @@
 #lang racket
-(require "main.rkt")
-(define dfa1 (make-ndfa '(A B C)
-                       '(a b c)
-                       'A
-                       '(B C)
-                       (list '(A b C)
-                             '(A b C)
-                             '(A c B)
-                             '(A c A))))
- 
+
+(require fsm)
+
+;; TEST MACHINES BELOW
+(define INIT-STATES '(A B C D))
+(define INIT-START 'A)
+(define INIT-FINALS '(C D))
+(define INIT-RULES (list '(A a B) '(B b A) '(A c C) '(C b D)))
+(define INIT-SIGMA '(a b c b))
+(define INIT-CURRENT 'A)
+(define INIT-ALPHA '(a b c))
+
+(define M1 (make-dfa INIT-STATES INIT-ALPHA INIT-START INIT-FINALS INIT-RULES))
+
+;; This machine works
+(define M2 (make-dfa '(A B C)
+                     '(a b c)
+                     'A
+                     '(B C)
+                     (list '(A b C)
+                           '(A a B)
+                           '(B c A))))
+(define M3 (make-dfa '(A B C)
+                     '(a b c)
+                     'A
+                     '(B C)
+                     (list '(B b C)
+                           '(C a B)
+                           '(B c B))))
