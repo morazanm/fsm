2ffb99a44cb9731a168cfc8bd2184d334fc5f1f5
Joshua Schappel <jmschappel12@gmail.com>
2019-10-08 14:22:49 -0400

    M1 is now a ndfa

--- testing-file.rkt
+++ testing-file.rkt
@@ -6,12 +6,12 @@
 (define INIT-STATES '(A B C D))
 (define INIT-START 'A)
 (define INIT-FINALS '(C D))
-(define INIT-RULES (list '(A a B) '(B b A) '(A c C) '(C b D)))
+(define INIT-RULES (list `(A ,EMP B) '(B b A) '(A c C) '(C b D)))
 (define INIT-SIGMA '(a b c b))
 (define INIT-CURRENT 'A)
-(define INIT-ALPHA '(a b c))
+(define INIT-ALPHA `(a b c))
 
-(define M1 (make-dfa INIT-STATES INIT-ALPHA INIT-START INIT-FINALS INIT-RULES))
+(define M1 (make-ndfa INIT-STATES INIT-ALPHA INIT-START INIT-FINALS INIT-RULES))
 
 ;; This machine works
 (define M2 (make-dfa '(A B C)
