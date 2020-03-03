d949badeb19baa765b7d68a1d9807d84442bc94e
morazanm <morazanm@gmail.com>
2020-03-03 15:45:36 -0500

    added sm-getaccept

--- fsm-main.rkt
+++ fsm-main.rkt
@@ -27,6 +27,7 @@
  ; sm observers
  sm-apply sm-showtransitions sm-type
  sm-getstates sm-getalphabet sm-getrules sm-getfinals sm-getstart sm-getstackalphabet
+ sm-getaccept
 
  ; sm testers
  sm-sameresult? sm-testequiv? sm-test
