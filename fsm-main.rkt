6d53ae9ee9818a93277ab74d9755f092945a0c25
morazanm <morazanm@gmail.com>
2022-08-23 18:21:57 -0400

    Fix to sm-intersection

--- fsm-main.rkt
+++ fsm-main.rkt
@@ -150,13 +150,11 @@
 (define (sm-intersection m1 m2)
   (let ((t1 (sm-type m1))
         (t2 (sm-type m2)))
-    (cond [(not (eq? t1 t2)) 
-           (error "Machines have different types: intersection-fsm")]
+    (cond [(and (or (eq? t1 'dfa) (eq? t1 'ndfa))
+                (or (eq? t2 'dfa) (eq? t2 'ndfa)))
+           (intersection-fsa m1 m2)]
           [(eq? t1 'pda)
            (error "Cannot intersect two pdas")]
-          [(or (eq? t1 'dfa)
-               (eq? t2 'ndfa))
-           (intersection-fsa m1 m2)]
           [(and (eq? t1 'tm-language-recognizer) (eq? t1 t2)) (tm-intersection m1 m2)]
           [(or (eq? t1 'mttm) (eq? t1 'mttm-language-recognizer))
            (error "Intersection not supported for multitape Turing machines")]
