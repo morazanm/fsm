918f3d218e86ce908475e0a53a58a6535bc15b43
jschappel <jmschappel12@gmail.com>
2022-09-05 10:52:55 -0400

    fix: fixed image scaler

--- FSM-Visualization/graphViz/main.rkt
+++ FSM-Visualization/graphViz/main.rkt
@@ -1,35 +1,61 @@
 #lang racket
-(require 2htdp/image "../globals.rkt" "../../GraphViz/lib.rkt" "../../GraphViz/render-graph.rkt"
-         "../structs/state.rkt" "../structs/machine.rkt" "../inv.rkt")
+(require 2htdp/image
+         "../inv.rkt"
+         "../globals.rkt"
+         "../structs/state.rkt"
+         "../structs/machine.rkt"
+         "../../GraphViz/lib.rkt"
+         "../../GraphViz/render-graph.rkt")
 
-(provide scaled-graph create-png)
+(provide scaled-graph create-png resize-image)
 
+;; resize-image :: image -> int -> int -> image
+;; Scales a image to the given dimentions. This solution was adapted from
+;; one of the answers found here: https://stackoverflow.com/questions/3008772/how-to-smart-resize-a-displayed-image-to-original-aspect-ratio
+(define (resize-image img max-width max-height)
+  (define src-width (image-width img))
+  (define src-height (image-height img))
+  (define resize-width src-width)
+  (define resize-height src-height)
+  (define aspect (/ resize-width resize-height))
+  (define scale (min
+                 (/ max-width src-width) ; scale-x
+                 (/ max-height src-height))) ;scale-y
 
+  (set! resize-width (* resize-width scale))
+  (set! resize-height (* resize-height scale))
+  
+  (when (> resize-width max-width)
+    (set! resize-width max-width)
+    (set! resize-height (/ resize-width aspect)))
+
+  (when (> resize-height max-height)
+    (set! aspect (/ resize-width resize-height))
+    (set! resize-height max-height)
+    (set! resize-width (* resize-height aspect)))
+
+  (scale/xy
+   (/ resize-width src-width)
+   (/ resize-height src-height)
+   img))
+
+;; scaled-graph :: image -> machine-type -> image
+;; scales a image base on the given machine type
 (define (scaled-graph img type)
-  (let* ([w-with-left-btns (- w 50)]
-         [smallest (>= (- w-with-left-btns (image-width img))
-                       (- h (image-height img)))]
-         [allocated-w (if (eq? 'pda type) (/ w-pda 2)(/ w 2))]
-         [allocated-h (if (eq? 'pda type) (/ h-pda 2)(/ h 2))])
-    (cond
-      [(and (< (image-width img) allocated-w)
-            (< (image-height img) allocated-h))
-       (define new-img (scale SMALL-IMG-SCALE img))
-       ;; if the newly scaled img is larger then the allocated space we will use the old one
-       (if (or (> (image-width new-img) allocated-w)
-               (> (image-height new-img) allocated-h))
-           img
-           new-img)]
-      [smallest
-       (if (equal? type 'pda)
-           (scale (floor (/ h-pda (image-height img))) img)
-           (scale (floor (/ w-with-left-btns (image-width img))) img))]
-      [else
-       (if (equal? type 'pda)
-           (scale (/ w-pda (image-width img)) img)
-           (scale (/ w-with-left-btns (image-width img)) img))])))
+  (define image-area-width (if (eq? 'pda type) w-pda w))
+  (define image-area-height (if (eq? 'pda type) h-pda h))
+  ;; if the image fits on the screen by default we will not resize
+  ;; becase it can effect the aspect ratio. In the future we may want to change this.
+  ;; see: https://graphviz.org/docs/attrs/ratio/ for more details on img resizing
+  (if (or (> (image-width img) image-area-width)
+          (> (image-height img) image-area-height))
+      (resize-image img image-area-width image-area-height)
+      img))
 
-
+;; create-png :: machine -> bool -> symbol -> rule -> image
+;; converts a machine to a graphviz internal representaion of a machine and
+;; calls the gviz library to convert it to a png. Then we scale it to the viztool
+;; if necessary.
 (define (create-png machine hasRun? cur-state cur-rule)
   (letrec ((g (create-graph 'G #:color 0))
            (states (machine-state-list machine))
@@ -62,6 +88,3 @@
       (if (or (member 'empty cur-rule) (member 'null cur-rule))
           (scaled-graph (graph->png g) MACHINE-TYPE)
           (scaled-graph (graph->png g #:rule cur-rule) MACHINE-TYPE)))))
-;(scaled-graph (bitmap/file (string-append (path->string (current-directory)) "vizTool.png")) MACHINE-TYPE))))
-  
-
