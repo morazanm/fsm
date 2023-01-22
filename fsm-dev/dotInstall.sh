0d5e049b840c932e83568f24dd08c21a348fe5e4
jschappel <jmschappel12@gmail.com>
2023-01-22 11:20:55 -0500

    feat(script): More work done on script

--- fsm-dev/dotInstall.sh
+++ fsm-dev/dotInstall.sh
@@ -1,22 +1,57 @@
 #!/bin/bash 
+
+set -e
+set -o pipefail
+set -u
+
 OS=$(uname)
+DOWNLOAD_SITE=https://graphviz.org/download/
+BLUE='\033[1;34m'
+NC='\033[0m' # No Color
+
+
 function testcmd() {
-command -v "$1" >/dev/null
+  command -v "$1" >/dev/null
 }
 
 function installMacOS() {
   if testcmd brew; then
     brew install graphviz
+    echo -e "n\Graphviz is now installed"
   elif testcmd port; then
     sudo port install graphviz
+    echo -e "\nGraphviz is now installed"
   else
-    echo "Please install homebrew or macPorts and try again"
+    echo "Please install homebrew or macPorts package manager and try again"
     exit 1
   fi
 }
 
 function installLinux() {
-  echo "TODO LINUX"
+  PS3="Enter the number retailed to your linux distrbution: "
+  select distro in "Debian/Ubuntu" "Fedora/CentOS" "Arch" "Other"
+  do
+    case $distro in
+      "Debian/Ubuntu")
+        echo -e "\nPlease run the below command in your terminal:"
+        printf "\t${BLUE}sudo apt install graphviz${NC}\n"
+        break;;
+      "Fedora/CentOS")
+        echo -e "\nPlease run the below command in your terminal:"
+        printf "\t{$BLUE}sudo yum install graphviz${NC}\n"
+        break;;
+      "Arch")
+        echo -e "\nPlease run the below command in your terminal:"
+        printf "\t${BLUE}sudo pacman -Sy graphviz${NC}\n"
+        break;;
+      "Other")
+        echo -e "\nPlease visit ${BLUE}${DOWNLOAD_SITE}${NC} to install graphviz on your machine"
+        break;;
+      *)
+        echo "Invalid option $REPLY"
+        ;;
+    esac
+  done
 }
 
 if testcmd dot; then
@@ -26,5 +61,7 @@ elif [ "$OS" == "Darwin" ]; then
 elif [ "$OS" == "Linux" ]; then
   installLinux
 else
-  echo "OS: $OS is not supported for install script"
+  echo "OS: $OS is not supported for install script. Please vist ${BLUE}${DOWNLOAD_SITE}${NC} to install graphviz on your machine"
 fi
+
+exit 0
