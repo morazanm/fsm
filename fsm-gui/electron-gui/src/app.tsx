5dbfab085206999e9d44003732d82cee93f53a9e
jschappel <jmschappel12@gmail.com>
2023-03-22 16:52:30 -0400

    feat(components): working on add/remove components

--- fsm-gui/electron-gui/src/app.tsx
+++ fsm-gui/electron-gui/src/app.tsx
@@ -1,14 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Button from '@mui/material/Button';
+import ReactDOM from 'react-dom/client';
+import AddRemoveForm from './components/AddRemoveForm';
+import AddRemoveStateForm from './components/AddRemoveStateForm';
+import { Divider, Stack } from '@mui/material';
 
 const App = () => {
   return (
-    <div>
-      <p>Hello from React</p>
-      <Button variant="contained">Click Me</Button>
+    <div style={{}}>
+      <Stack
+        spacing={4}
+        divider={<Divider orientation="horizontal" flexItem />}
+      >
+        <AddRemoveStateForm
+          onDelete={(_) => false}
+          onSubmit={(_) => false}
+          validate={(_) => true}
+        />
+        <AddRemoveForm
+          label="Alpha"
+          onDelete={(_) => false}
+          onSubmit={(_) => false}
+          validate={(_) => true}
+        />
+      </Stack>
     </div>
   );
 };
 
-ReactDOM.render(<App />, document.body);
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
---
+++ fsm-gui/electron-gui/src/components/AddRemoveRuleForm.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AddRemoveRuleForm = () => {
+  return <p>hi</p>;
+};
+
+export default AddRemoveRuleForm;
