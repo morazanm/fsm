8a401d0ca5a9c81a3a89f105653cfe6efc5a2f95
Josephine Des Rosiers <35999990+josdes@users.noreply.github.com>
2023-07-01 16:28:19 -0400

    addition
    here

--- fsm-core/private/macros/contracts.rkt
+++ fsm-core/private/macros/contracts.rkt
@@ -10,4 +10,22 @@
        [result boolean?])  
   (list? x))
 
+(define (valid-list-of-states? states)
+  )
+(define (valid-sigma? sigma)
+  )
+(define (valid-start? start)
+  )
+
+(define/contract (make-dfa states sigma start)
+  (->i ([states (and/c list?
+                       valid-list-of-states?)]
+        [sigma (and/c list?
+                      valid-sigma?]
+        [start (and/c symbol?
+                      valid-start?])
+       [result list?])
+  '(states sigma start)
+  )
+
 (f (list 1 2 3) 1)
