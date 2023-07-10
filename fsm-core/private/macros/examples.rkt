11120944afc2a6e72213239f99072e5d3d634878
morazanm <morazanm@gmail.com>
2023-07-10 01:16:21 -0400

    test for checking errors

--- fsm-core/private/macros/examples.rkt
+++ fsm-core/private/macros/examples.rkt
@@ -3,6 +3,7 @@
          "../constants.rkt"
          "../fsa.rkt"
          "../../../main.rkt")
+(local-require test-engine/racket-tests)
 
 
 (define dfa-temp (make-dfa2
@@ -13,9 +14,25 @@
                   (list '(A a A)
                         '(B a A)
                         '(B b B)
-                        )
+                        '(A b B))
                   #f
                   #:accepts (list '(a a a) '(a a))
                   #:rejects (list '(b b b b) '(b b))
                   ))
 (dfa-temp '(a a a a b))
+
+(check-error
+ (make-dfa2
+  `(A B)
+  '(a b)
+  'A
+  '(A)
+  (list '(A a A)
+        '(B a A)
+        '(B b B)
+        '(A b B))
+  #f
+  #:accepts (list '(a a a b) '(a a))
+  #:rejects (list '(b b b b) '(b b))
+  )
+ "Does not accept the predicted value:  ((a a a b))")
