8b2911034a2eeb2d24a0fe7882fbcbbaea1d4383
morazanm <morazanm@gmail.com>
2023-07-16 08:53:02 -0400

    mttm bug fix
    Extended previous fix for mttms to fsm-core's mtape: all computations are explored for lang recognizers before rejecting

--- fsm-core/private/macros/mtape-tm.rkt
+++ fsm-core/private/macros/mtape-tm.rkt
@@ -41,8 +41,7 @@
                   (cond [(void? accept-state) currpath] ;; if not a lang recog halt
                         [(eq? currstate accept-state)   ;; if lang recog add 'accept
                          (cons 'accept currpath)]
-                        [else (run (rest paths))
-                              #;(cons 'reject currpath)]) ;; else add 'reject
+                        [else (run (rest paths))]) ;; explore the rest of the computations
                   (let* ((pos-tapes (rest (first currpath)))
                          ;(ddd (display (format "pos-tapes: ~s\n" pos-tapes)))
                          (posns (map first pos-tapes))
