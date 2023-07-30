bba4a8d3759af817f2aca285341698ca7188fed7
Shamil Dzhatdoyev <shamil.d11@gmail.com>
2023-07-30 17:27:02 -0400

    Update bug list

--- fsm-core/private/macros/BugList.rkt
+++ fsm-core/private/macros/BugList.rkt
@@ -5,13 +5,9 @@
 ;;; pinpoint where in the rule we're finding issue
 ;;;;; ie. break it up by state, alphabet, action etc
 ;;; rules should be seperated by newlines
-;;; need a contract for the number of tapes on a mttm
 
 ;rules-predicates
 ;o Should we check to make sure everything in the list is a symbol
 
 ;validation-flat-contracts
 ;o error message for validation words in sigma is ugly
-
-;validation
-;o turing machine must specify where on the tape its starting with each word
