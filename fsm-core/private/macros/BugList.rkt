2a23c4fbf0f5ad0c0b630f5e75e8f25a410a9db5
Josephine Des Rosiers <35999990+josdes@users.noreply.github.com>
2023-07-30 15:00:14 -0400

    should accept blank and the lm

--- fsm-core/private/macros/BugList.rkt
+++ fsm-core/private/macros/BugList.rkt
@@ -2,7 +2,8 @@
 ;;BUG LIST
 
 ;TOOD:
-;;; pinpoint where in the rule the action fails
+;;; pinpoint where in the rule we're finding issue
+;;;;; ie. break it up by state, alphabet, action etc
 
 
 ;shared-predicates
