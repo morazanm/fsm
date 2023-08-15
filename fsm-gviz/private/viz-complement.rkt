f31d2dc19f07963c0bafbf92dce37c9d03a7a4d6
tijanaminic1 <63131835+tijanaminic1@users.noreply.github.com>
2023-08-14 23:19:06 -0400

    viz-complement pushed again

--- fsm-gviz/private/viz-complement.rkt
+++ fsm-gviz/private/viz-complement.rkt
@@ -34,17 +34,6 @@
                               (B a C)
                               (C b F)
                               (F b F))))
-
-(define M (make-ndfa '(Z H B C D)
-                     '(a b)
-                     'Z
-                     '(C D)
-                     `((Z a H)
-                       (Z a B)
-                       (H a D)
-                       (B a C))))
-
-
 ;; L = aab*
 (define aab* (make-ndfa '(W X Y)
                         '(a b)
@@ -197,4 +186,4 @@
                        (H a D)
                        (B a C))))
 
-(run M)
+
