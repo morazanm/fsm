959922086c0c74af10befe3407a6f51392e729d0
morazanm <morazanm@gmail.com>
2023-09-23 15:20:28 -0400

    Created an-b2n

---
+++ fsm-core/private/macros/an-b2n.rkt
@@ -0,0 +1,19 @@
+#lang fsm
+
+(require "constructors.rkt")
+
+;; L = a^nb^2n
+(define P (make-ndpda2 '(S F)
+                       '(a b)
+                       '(a)
+                       'S
+                       '(F)
+                       `(((S a ,EMP) (S (a a)))
+                         ((s ,EMP ,EMP) (F ,EMP))
+                         ((f b (A)) (F ,EMP)))))
+
+(check-equal? (sm-apply P '(b a)) 'reject)
+(check-equal? (sm-apply P '(a a a)) 'reject)
+(check-equal? (sm-apply P '(a b b a a b b b b)) 'reject)
+(check-equal? (sm-apply P '()) 'accept)
+(check-equal? (sm-apply P '(a a b b b b)) 'accept)
