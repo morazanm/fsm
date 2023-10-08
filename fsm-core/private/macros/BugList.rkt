787458351073535e26e88d74a13dd70de684f337
Josephine Des Rosiers <35999990+josdes@users.noreply.github.com>
2023-10-08 14:40:42 -0400

    update buglist
    update

--- fsm-core/private/macros/BugList.rkt
+++ fsm-core/private/macros/BugList.rkt
@@ -3,3 +3,5 @@
 
 ;TOOD:
 ;;; final states and rules must be able to be empty
+;;; get rid of space before "the following" after the newline and change it in all the tests
+;;; in the duplicates, give it the following values __ are duplicated in the ___ : ___
