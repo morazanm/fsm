8dbfa47b666f6bbe4b57e694526ecef081b160a1
Marco T. Morazan <morazanm@gmail.com>
2023-12-05 08:55:37 -0500

    swap ctm added to testing file

--- fsm-gviz/private/callgraphs/testing-file.rkt
+++ fsm-gviz/private/callgraphs/testing-file.rkt
@@ -54,3 +54,61 @@
                                RGHT
                                20)
                          '(i)))
+
+;;; SWAP MACHINE
+
+;;  PRE: tape = (LMw) AND i=k>0 AND w in (a b)*
+;; POST: tape = (LMw) AND i=k+1 AND w in (a b)*
+(define RS (make-tm '(S F)
+                    '(a b)
+                    `(((S a) (F ,RIGHT))
+                      ((S b) (F ,RIGHT))
+                      ((S ,BLANK) (F ,RIGHT)))
+                    'S
+                    '(F)))
+
+(check-equal? (sm-showtransitions RS `(,LM a b a) 1)
+              `((S 1 (,LM a b a))
+                (F 2 (,LM a b a))))
+(check-equal? (sm-showtransitions RS `(,LM a b a) 3)
+              `((S 3 (,LM a b a))
+                (F 4 (,LM a b a ,BLANK))))
+(check-equal? (second (last (sm-showtransitions RS `(,LM b b a a) 3)))
+              4)
+
+;;  PRE: tape = (LMw) AND i=k>0, where w in (a b)*
+;; POST: tape = (LMw) AND i=k+1
+(define LS (make-tm '(S H)
+                    `(a b)
+                    `(((S a) (H ,LEFT))
+                      ((S b) (H ,LEFT))
+                      ((S ,BLANK) (H ,LEFT)))
+                    'S
+                    '(H)))
+
+(check-equal? (last (sm-showtransitions LS `(,LM a a) 1))
+              `(H 0 (,LM a a)))
+(check-equal? (last (sm-showtransitions LS `(,LM ,BLANK b b a) 4))
+              `(H 3 (,LM ,BLANK b b a)))
+
+(define swap (combine-tms (list (list (list VAR 'i)
+                                      RS
+                                      (list (list VAR 'j)
+                                            'i
+                                            LS
+                                            'j)))
+                          '(a b)))
+
+(define swapL '(((VAR i)
+                 RS
+                 ((list VAR j)
+                  i
+                  LS
+                  j))))
+
+(check-equal? (rest (ctm-run swap `(,LM ,BLANK a b a) 3))
+              `(3 (,LM ,BLANK a a b)))
+(check-equal? (rest (ctm-run swap `(,LM ,BLANK a b a a) 5))
+              `(5 (@ _ a b a _ a)))
+
+;;; END SWAP MACHINE
