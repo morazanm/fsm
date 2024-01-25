602f43290d0f7e8d2dbcb2fc03035bb3ee476d33
Josephine Des Rosiers <35999990+josdes@users.noreply.github.com>
2024-01-24 22:25:35 -0500

    here
    you go

--- fsm-test/fsm-core/test-temp.rkt
+++ fsm-test/fsm-core/test-temp.rkt
@@ -7,9 +7,16 @@
 (module+ test
   (require rackunit)
   
-  ; L = a*
   (define dfa-a* (make-dfa '(Q) '(a b) 'Q '(Q) '((Q a Q))))
-  
-  ;; ---- DFA TESTS ---
-  (define dfa-renamests-a* (sm-rename-states (sm-states dfa-a*) dfa-a*))
+  (define dfa-b* (make-dfa '(Q) '(a b) 'Q '(Q) '((Q b Q))))
+  (define ndfa-inter-a*-b* (sm-intersection dfa-a* dfa-b*))
+  (sm-finals ndfa-inter-a*-b*)
+
+  (check-equal? (sm-apply ndfa-inter-a*-b* ' (a b a a b)) 'reject)
+  (check-equal? (sm-apply ndfa-inter-a*-b* ' (b b b)) 'reject)
+  (check-equal? (sm-apply ndfa-inter-a*-b* ' (a a a)) 'reject)
+  (check-equal? (sm-apply ndfa-inter-a*-b* ' (a b)) 'reject)
+  (check-equal? (sm-apply ndfa-inter-a*-b* ' (a a a b ))'reject)
+  (check-equal? (sm-apply ndfa-inter-a*-b* ' ()) 'accept)
+
   )
