55bc1a45fbdd018eab7cfe84b2fb44f776b34e4a
Marco T. Morazan <morazanm@gmail.com>
2024-01-24 19:08:58 -0500

    define contract for make-dfa2

--- fsm-core/private/macros/constructors.rkt
+++ fsm-core/private/macros/constructors.rkt
@@ -40,6 +40,59 @@
       )
     (append rules dead-state-rules))
 
+  (define make-dfa/c (->i ([states (and/c (is-a-list/c "machine states" "three")
+                                          (valid-listof/c valid-state? "machine state" "list of machine states" #:rule "three")
+                                          (no-duplicates/c "states"))]
+                           [sigma (and/c (is-a-list/c "machine alphabet" "one")
+                                         (valid-listof/c valid-alpha? "lowercase alphabet letter" "input alphabet" #:rule "one")
+                                         (no-duplicates/c "sigma"))]
+                           [start (states) (and/c (valid-start/c states)
+                                                  (start-in-states/c states))]
+                           [finals (states) (and/c (is-a-list/c "machine final states" "three")
+                                                   (valid-listof/c valid-state? "machine state" "list of machine finals" #:rule "three")
+                                                   (valid-finals/c states)
+                                                   (no-duplicates/c "final states"))]
+                           [rules (states
+                                   sigma
+                                   add-dead) (and/c (is-a-list/c "machine rules" "four")
+                                                    correct-dfa-rule-structures/c
+                                                    (correct-dfa-rules/c states sigma)
+                                                    (functional/c states sigma add-dead)
+                                                    (no-duplicates-dfa/c "rules"))]
+                           )
+                          ([add-dead boolean?]
+                           #:accepts [accepts (states
+                                               sigma
+                                               start
+                                               finals
+                                               rules
+                                               add-dead) (and/c (words-in-sigma/c sigma)
+                                                                (listof-words/c "accpets")
+                                                                (dfa-input/c states
+                                                                             sigma
+                                                                             start
+                                                                             finals
+                                                                             rules
+                                                                             add-dead
+                                                                             'accept))]
+                           #:rejects [rejects (states
+                                               sigma
+                                               start
+                                               finals
+                                               rules
+                                               add-dead) (and/c (words-in-sigma/c sigma)
+                                                                (listof-words/c "rejects")
+                                                                (dfa-input/c states
+                                                                             sigma
+                                                                             start
+                                                                             finals
+                                                                             rules
+                                                                             add-dead
+                                                                             'reject))]
+                           )
+         
+                          [result dfa?]))
+
   ;; make-dfa: states alphabet state states rules (boolean) -> machine
   ;; Purpose: Eventually, will construct a multi-tape turing-machine from the given
   ;; DFA inputs, but for now just parses inputs and constructs an unchecked-dfa.
@@ -47,7 +100,8 @@
                               [add-dead #t]
                               #:accepts [accepts '()]
                               #:rejects [rejects '()])
-    (->i ([states (and/c (is-a-list/c "machine states" "three")
+    make-dfa/c
+    #;(->i ([states (and/c (is-a-list/c "machine states" "three")
                          (valid-listof/c valid-state? "machine state" "list of machine states" #:rule "three")
                          (no-duplicates/c "states"))]
           [sigma (and/c (is-a-list/c "machine alphabet" "one")
@@ -125,7 +179,7 @@
                                   (no-duplicates/c "final states"))]
           [rules (states
                   sigma) (and/c (is-a-list/c "machine rules" "four")
-                          correct-dfa-rule-structures/c
+                                correct-dfa-rule-structures/c
                                 (correct-dfa-rules/c states (cons EMP sigma))
                                 (no-duplicates/c "rules"))]
           )
