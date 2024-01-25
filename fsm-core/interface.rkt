b85d92516d09a7fd63900c63b374dc5f912841f1
Josephine Des Rosiers <35999990+josdes@users.noreply.github.com>
2024-01-24 22:39:30 -0500

    its inverse
    now

--- fsm-core/interface.rkt
+++ fsm-core/interface.rkt
@@ -501,8 +501,8 @@
     make-dfa/c
     (define all-rules
       (if add-dead
-          (add-dead-state-rules rules states sigma)
-          rules))
+          rules
+          (add-dead-state-rules rules states sigma)))
     (if add-dead (make-unchecked-dfa states sigma start finals all-rules add-dead)
         (make-unchecked-dfa states sigma start finals all-rules))
     )
