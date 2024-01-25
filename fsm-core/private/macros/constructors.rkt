046fb5ee278993eb67c7065b11324ae4219aef1f
Josephine Des Rosiers <35999990+josdes@users.noreply.github.com>
2024-01-24 20:19:18 -0500

    nodead to no-dead
    cause apparently thats how its supposed to be -_-

--- fsm-core/private/macros/constructors.rkt
+++ fsm-core/private/macros/constructors.rkt
@@ -57,7 +57,7 @@
                                                     (functional/c states sigma add-dead)
                                                     (no-duplicates-dfa/c "rules"))]
                            )
-                          ([add-dead (lambda (x) (equal? 'nodead x))]
+                          ([add-dead (lambda (x) (equal? 'no-dead x))]
                            #:accepts [accepts (states
                                                sigma
                                                start
