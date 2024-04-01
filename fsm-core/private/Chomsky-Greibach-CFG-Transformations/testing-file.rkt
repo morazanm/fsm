33331009d4a721f0662816432b02c9667e1ca944
Marco T. Morazan <morazanm@gmail.com>
2024-04-01 19:48:23 -0400

    added anbn to testing file

--- fsm-core/private/Chomsky-Greibach-CFG-Transformations/testing-file.rkt
+++ fsm-core/private/Chomsky-Greibach-CFG-Transformations/testing-file.rkt
@@ -2,3 +2,27 @@
 
 (require "transformations.rkt")
 
+(define anbn (make-cfg '(S)
+                       '(a b)
+                       `((S ,ARROW ,EMP)
+                         (S ,ARROW aSb))
+                       'S))
+
+(check-equal? (grammar-derive anbn '()) "The word () is too short to test.")
+(check-equal? (last (grammar-derive anbn '(a a a b b b))) 'aaabbb)
+(check-equal? (last (grammar-derive anbn '(a b))) 'ab)
+(check-equal? (last (grammar-derive anbn '(a a a a a a b b b b b b))) 'aaaaaabbbbbb)
+
+(define anbn-chomsky (chomsky anbn))
+
+(check-equal? (grammar-derive anbn-chomsky '()) "The word () is too short to test.")
+(check-equal? (last (grammar-derive anbn-chomsky '(a a a b b b))) 'aaabbb)
+(check-equal? (last (grammar-derive anbn-chomsky '(a b))) 'ab)
+(check-equal? (last (grammar-derive anbn-chomsky '(a a a a a a b b b b b b))) 'aaaaaabbbbbb)
+
+(define anbn-greibach (greibach anbn))
+
+(check-equal? (grammar-derive anbn-greibach '()) "The word () is too short to test.")
+(check-equal? (last (grammar-derive anbn-greibach '(a a a b b b))) 'aaabbb)
+(check-equal? (last (grammar-derive anbn-greibach '(a b))) 'ab)
+(check-equal? (last (grammar-derive anbn-greibach '(a a a a a a b b b b b b))) 'aaaaaabbbbbb)
