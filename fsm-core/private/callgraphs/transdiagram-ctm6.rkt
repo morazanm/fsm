33dea434ee0c658a9c633e926918f6fe8e71c114
oliwial23 <oliwia1406@gmail.com>
2024-04-22 18:01:33 -0400

    mau

--- fsm-core/private/callgraphs/transdiagram-ctm6.rkt
+++ fsm-core/private/callgraphs/transdiagram-ctm6.rkt
@@ -449,13 +449,13 @@
                      (follow-trace (cdr trace) edges (if (empty? new-edge)
                                                          ""
                                                          (cadr (car new-edge))))))]
-          [(or (equal? 'GOTO (car (car trace)))
-               (equal? 'BRANCH (car (car trace)))
-               (equal? 'VAR (car (car trace))))
+          [else 
            (follow-trace (cdr trace) edges stored-val)]))
-  (follow-trace (cdr (ctm-apply ctm tape head #t))
+  (follow-trace (filter (lambda (x) (or (struct? x)
+                                        (and (not (equal? (car x) 'GOTO))
+                                             (not (equal? (car x) 'VAR))))) (cdr (ctm-apply ctm tape head #t)))
                 (filter (lambda (x) (not (equal? "white" (cadr (caddr (caddr x)))))) (clean-list (dot-edges (parse-program ctmlist))))
                 (car (car (clean-list (dot-edges (parse-program ctmlist)))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
+  
