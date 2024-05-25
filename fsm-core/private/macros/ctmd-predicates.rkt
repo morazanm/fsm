020c103c7343002167e1e185bae6aa526899945c
Shamil Dzhatdoyev <shamil.d11@gmail.com>
2024-05-25 16:48:48 -0400

    Verify that variables are in scope
    When a variable is encountered in a CTMD, we now validate that the symbol exists in the current scope (based on previously defined VARs)

--- fsm-core/private/macros/ctmd-predicates.rkt
+++ fsm-core/private/macros/ctmd-predicates.rkt
@@ -156,7 +156,7 @@
   ;;    a valid BRANCH expression
   ;;    a valid VAR declaration followed by a valid CTMD
   ;;    a full valid ctmd or valid tm
-  (define (valid-list-case? input labels sigma)
+  (define (valid-list-case? input labels sigma variables)
     (cond [(equal? GOTO (car input)) (if (not (= (length input) 2)) "A GOTO expression must be of length 2"
                                          (if (in-labels? (cadr input) labels) #t
                                              "The second part of GOTO must be a label that exists in your machine"))] ;;; must be an existing label
@@ -166,54 +166,76 @@
                                                  (if (empty? all-invalid-branches) #t
                                                      (format "The following branches have errors: ~a" all-invalid-branches)))))]
           [(and (list? (car input))
-                (equal? (car (car input)) VAR)) (if (not (= (length (car input)) 2)) "A VAR declaration must be of length 2"
-                                                    (if (not (symbol? (cadr (car input)))) "The second part of a VAR declaration must be a symbol"
-                                                        (valid-ctmd? (cadr input) labels sigma)))]
-          [else (valid-ctmd? input labels sigma)]
+                (equal? (car (car input)) VAR)) (if (not (= (length (car input)) 2))
+                                                    "A VAR declaration must be of length 2"
+                                                    (if (not (symbol? (cadr (car input))))
+                                                        "The second part of a VAR declaration must be a symbol"
+                                                        (valid-ctmd? (cadr input) labels sigma (cons (cadr (car input)) variables))))]
+          [else (valid-ctmd? input labels sigma variables)]
           ))
 
-  (check-expect (valid-list-case? `(,GOTO 10) '(10) '()) #t)
-  (check-expect (valid-list-case? `(,GOTO 20 30) '(20) '()) "A GOTO expression must be of length 2")
-  (check-expect (valid-list-case? `(,GOTO 'a) '() '()) "The second part of GOTO must be a label that exists in your machine")
+  (check-expect (valid-list-case? `(,GOTO 10) '(10) '() '()) #t)
+  (check-expect (valid-list-case? `(,GOTO 20 30) '(20) '() '()) "A GOTO expression must be of length 2")
+  (check-expect (valid-list-case? `(,GOTO 'a) '() '() '()) "The second part of GOTO must be a label that exists in your machine")
 
   (check-expect (valid-list-case? `(,BRANCH ((a (,GOTO 10))
-                                             (b (,GOTO 20)))) '(10 20) '(a b))
+                                             (b (,GOTO 20)))) '(10 20) '(a b) '())
                 #t)
   (check-expect (valid-list-case? `(,BRANCH (a (,GOTO 10))
-                                            (b (,GOTO 20))) '(10 20) '(a b))
+                                            (b (,GOTO 20))) '(10 20) '(a b) '())
                 "A BRANCH expression must be of length 2")
-  (check-expect (valid-list-case? `(,BRANCH a) '() '(a))
+  (check-expect (valid-list-case? `(,BRANCH a) '() '(a) '())
                 "The second part of a BRANCH must be a list")
   (check-expect (valid-list-case? `(,BRANCH ((a (,GOTO a))
-                                             (b (,GOTO 20)))) '(20) '(a b))
+                                             (b (,GOTO 20)))) '(20) '(a b) '())
                 "The following branches have errors: ((a (GOTO a)))")
+  (check-expect (valid-list-case? `((,VAR a) (b)) '() '() '())
+                "The variable being referenced is not defined in this scope.")
+
+  ; valid-variable?: symbol (list of symbols) -> boolean
+  ; Purpose: Returns true if the variable is in the given list, and false otherwise.
+  (define (valid-variable? variable variables)
+    (if (member variable variables) #t #f))
 
   ;; may need accumulator for variables defined in scope
-  (define (valid-ctmd? input labels sigma)
+  (define (valid-ctmd? input labels sigma variables)
     (cond [(empty? input) #t]
-          [(number? (car input)) (valid-ctmd? (cdr input) labels sigma)]
-          [(symbol? (car input)) (valid-ctmd? (cdr input) labels sigma)] ;; is symbol in accumulated lsit
-          [(list? (car input)) (let [(list-case (valid-list-case? (car input) labels sigma))]
+          [(list? (car input)) (let [(list-case (valid-list-case? (car input) labels sigma variables))]
                                  (if (string? list-case) list-case
-                                     (valid-ctmd? (cdr input) labels sigma)))]
+                                     (valid-ctmd? (cdr input) labels sigma variables)))]
+          [(number? (car input)) (valid-ctmd? (cdr input) labels sigma variables)]
+          [(symbol? (car input)) (if (not (valid-variable? (car input) variables))
+                                     "The variable being referenced is not defined in this scope."
+                                      (valid-ctmd? (cdr input) labels sigma variables))] ;; is symbol in accumulated list
           [(procedure? (car input)) (if (or (equal? (sm-type (car input)) 'tm)
                                             (equal? (sm-type (car input)) 'tm-language-recognizer))
-                                        (valid-ctmd? (cdr input) labels sigma)
+                                        (valid-ctmd? (cdr input) labels sigma variables)
                                         "Only machines allowed are turing machines")]
           )
     )
 
-  (check-expect (valid-ctmd? '() '() '()) #t)
-  (check-expect (valid-ctmd? '(5) '(5) '()) #t) ;; Rest of ctmd is empty, which is valid
-  (check-expect (valid-ctmd? '(sym) '() '()) #t) ;; Rest of ctmd is empty, which is valid
-  (check-expect (valid-ctmd? `((,GOTO 5) 's) '(5) '()) #t)
-  (check-expect (valid-ctmd? `((,BRANCH ((a (,GOTO 10)) (b (,GOTO 20)))) 'd) '(10 20) '(a b d)) #t)
-  (check-expect (valid-ctmd? `((,BRANCH ((a (,GOTO a)) (b (,GOTO 20)))) 'd) '(20) '(a b d))
+  (check-expect (valid-ctmd? '() '() '() '()) #t)
+  (check-expect (valid-ctmd? '(5) '(5) '() '()) #t) ;; Rest of ctmd is empty, which is valid
+  (check-expect (valid-ctmd? '(sym) '() '() '(sym)) #t) ;; Rest of ctmd is empty, which is valid
+  (check-expect (valid-ctmd? `((,GOTO 5) s) '(5) '() '(s)) #t)
+  (check-expect (valid-ctmd? `((,BRANCH ((a (,GOTO 10)) (b (,GOTO 20)))) d) '(10 20) '(a b d) '(d)) #t)
+  (check-expect (valid-ctmd? `((,BRANCH ((a (,GOTO a)) (b (,GOTO 20)))) 'd) '(20) '(a b d) '())
                 "The following branches have errors: ((a (GOTO a)))")
+  (check-expect (valid-ctmd? `(s) '() '() '()) "The variable being referenced is not defined in this scope.")
 
+  ; check-ctmd: any (list of symbol) -> true or string
+  ; Purpose: Parses the given input. If the input is an s-exp representing a valid
+  ; CTMD, then returns true. Otherwise, it returns a string containing an error message.
   (define (check-ctmd input sigma)
-    (let [(labels (gather-labels input))]
-      (valid-ctmd? input '() sigma)))
+    (let [(labels (gather-labels input))
+          (initial-variables '())]
+      (valid-ctmd? input labels sigma initial-variables)))
+
+  (check-expect (check-ctmd `(((,VAR a) (a))) '()) #t)
+  (check-expect (check-ctmd `(((,VAR a) (b))) '()) "The variable being referenced is not defined in this scope.")
+  (check-expect (check-ctmd `((, BRANCH ((a (,GOTO 10)) (b (,GOTO 20)))) 10 20) '(a b)) #t)
+  (check-expect (check-ctmd `((, BRANCH ((a (,GOTO 10)) (b (,GOTO 20)))) 10 230) '(a b))
+                "The following branches have errors: ((b (GOTO 20)))") ; TODO: Should improve this to incorporate the underlying error message.
   
   (test)
   )
