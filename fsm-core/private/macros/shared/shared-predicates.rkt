2fa14ca608230fdec6495304608ec7205b0c3738
Shamil Dzhatdoyev <shamil.d11@gmail.com>
2025-01-27 18:30:06 -0500

    Allow uppercase alphabet characters

--- fsm-core/private/macros/shared/shared-predicates.rkt
+++ fsm-core/private/macros/shared/shared-predicates.rkt
@@ -98,7 +98,7 @@
 ;purpose: takes in anything and makes sure that it is a symbol that
 ; a lowercase roman letter
 (define (valid-alpha? x)
-  (define regex-pattern (regexp "^[a-z0-9$&!*]$"))
+  (define regex-pattern (regexp "^[a-zA-Z0-9$&!*]$"))
   (and (or (symbol? x) (and (number? x) (<= 0 x 9)))
        (not (not (regexp-match regex-pattern
                                (if (symbol? x)
