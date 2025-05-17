fac6a4732faa5c390aa9f70218b206e77dd8e355
Marco T. Morazan <morazanm@gmail.com>
2025-05-16 20:15:49 -0400

    updated with Gina new code for inv testing

--- quickcheck-stuff/sm-test-invs-wip.rkt
+++ quickcheck-stuff/sm-test-invs-wip.rkt
@@ -788,6 +788,68 @@
 (define (path-inv-not-hold? a-path a-loi)
   (not (invariant-holds? a-loi (list (word-of-path a-path) (third (last a-path))))))
 
+;; machine (listof (list state (word -> boolean))) -> (listof (listof state (listof word)))
+;; Purpose: To return a list of all posible words that can be at each state in a machine 
+(define (sm-all-possible-words a-machine a-loi)
+  (local [;; the given machine without the states and rules of states that cannot reach a final state
+          (define new-machine (if (eq? (sm-type a-machine) 'dfa)
+                                  a-machine
+                                  (remove-states-that-cannot-reach-finals a-machine)))
+          ;; list of invariants that are reachable from the starting configuration
+          (define reachable-inv (filter (λ (x) (member? (first x) (sm-states new-machine))) a-loi))
+          ;; all paths of new-machine
+          (define all-paths-new-machine (find-paths new-machine))
+
+          ;; (listof (listof rule)) (listof (listof symbol)) -> (listof (listof symbol))
+          ;; Purpose:  To return a list of all posible words that can be at each state in a machine 
+          ;; Accumulator Invarient: accum = list of lists of words with the states that the can possibly be at
+          (define (sm-all-possible-words-helper all-paths accum)
+            (if (empty? all-paths)
+                accum
+                (sm-all-possible-words-helper (rest all-paths)
+                                              (cons (list (word-of-path (first all-paths))
+                                                          (third (last (first all-paths)))) accum))))
+
+          ;; (listof symbol) -> (listof (symbol (listof word)))
+          ;; Purpose: To generate a list of lists of a state and an empty list for each given states
+          (define (build-list-of-states-&-empty-low states)
+            (local [;; (listof symbol) (listof word)-> (listof (symbol '()))
+                    ;; Purpose: To generate a list of lists of a state and an empty list for each given states
+                    ;; Accumulator Invariant: accum = list containing a list with the state and an empty list of words
+                    (define (build-list-of-states-&-empty-low-helper states accum)
+                      (if (empty? states) accum
+                          (build-list-of-states-&-empty-low-helper (rest states) (cons (list (first states) '()) accum))))]
+              (build-list-of-states-&-empty-low-helper states '())))
+          
+          (define states-&-empty-low (build-list-of-states-&-empty-low (sm-states new-machine)))
+
+          ;; (listof (listof word state)) -> (listof (listof state (listof word)))
+          ;; Purpose: To sort the words to be a list of the state and the words that can possibly reach that state
+          (define (sort-words listof-all-words-&-states)
+            (local [;; (listof (word state)) -> (listof word)
+                    ;; Purpose: To make the given (listof (word state)) into a list of words
+                    (define (make-low list-of-words-&-states)
+                      (local [;; (listof (word state)) -> (listof word)
+                              ;; Purpose: To make the given (listof (word state)) into a list of words
+                              ;; Accumulator Invariant: accum = current list of words for the state
+                              (define (make-low-helper list-of-words-&-states accum)
+                                (if (empty? list-of-words-&-states) accum
+                                    (make-low-helper (rest list-of-words-&-states) (cons (first (first list-of-words-&-states)) accum))))]
+                        (make-low-helper list-of-words-&-states '())))
+                    ;; (listof (listof word state)) -> (listof (listof state (listof word)))
+                    ;; Purpose: To sort the words to be a list of the state and the words that can possibly reach that state
+                    ;; Accumulator Invairant: accum = list of states with all the possible words in them 
+                    (define (sort-words-helper states-&-empty-low accum)
+                      (if (empty? states-&-empty-low) accum
+                          (sort-words-helper (rest states-&-empty-low)
+                                             (cons (list (first (first states-&-empty-low))
+                                                         (make-low (filter (λ (x) (eq? (first (first states-&-empty-low)) (second x)))
+                                                                           listof-all-words-&-states))) accum))
+                          ))
+                    ]
+              (sort-words-helper states-&-empty-low '())))]
+    (sort-words (sm-all-possible-words-helper all-paths-new-machine
+                                              (list (list '() (sm-start a-machine)))))))
 
 ;; machine (listof (list state (word -> boolean))) -> (listof (listof symbol))
 ;; Purpose: To return a list of the invarients that don't hold and the words that cause it not to hold
@@ -822,3 +884,56 @@
                          (if ((second (assoc (sm-start a-machine) a-loi)) '())
                              '()
                              (list (list '() (sm-start a-machine)))))))
+
+
+(define M
+  (make-ndfa '(S A B C D E F)
+             '(a b c)
+             'S
+             '(F)
+             `((S ,EMP B) (S ,EMP A) (A a A) (A c D)
+                          (B b C) (C b B) (C c D) (D c E)
+                          (E c F) (F c D))))
+
+(define S2-INV empty?)
+
+(define (A2-INV ci)
+  (andmap (λ (s) (eq? s 'a)) ci))
+
+(define (B2-INV ci)
+  (and (even? (length ci))
+       (andmap (λ (s) (eq? s 'b)) ci)))
+
+(define (C2-INV ci)
+  (and (odd? (length ci))
+       (andmap (λ (s) (eq? s 'b)) ci)))
+
+(define (D2-INV ci)
+  (let* [(bcs (takef ci (λ (s) (not (eq? s 'c)))))
+         (cs (drop ci (length bcs)))]
+    (and (or (C2-INV bcs) (A2-INV bcs))
+         (andmap (λ (s) (eq? s 'c)) cs)
+         (= (remainder (length cs) 3) 1))))
+
+(define (E2-INV ci)
+  (let* [(bcs (takef ci (λ (s) (not (eq? s 'c)))))
+         (cs (drop ci (length bcs)))]
+    (and (or (C2-INV bcs) (A2-INV bcs))
+         (andmap (λ (s) (eq? s 'c)) cs)
+         (= (remainder (length cs) 3) 2))))
+
+(define (F2-INV ci)
+  (let* [(bcs (takef ci (λ (s) (not (eq? s 'c)))))
+         (cs (drop ci (length bcs)))]
+    (and (or (C2-INV bcs) (A2-INV bcs))
+         (andmap (λ (s) (eq? s 'c)) cs)
+         (= (remainder (length cs) 3) 0))))
+
+(sm-all-possible-words M
+              (list (list 'S S2-INV)
+                    (list 'A A2-INV)
+                    (list 'B B2-INV)
+                    (list 'C C2-INV)
+                    (list 'D D2-INV)
+                    (list 'E E2-INV)
+                    (list 'F F2-INV)))
