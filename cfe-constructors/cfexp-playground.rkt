b2b71ef7d9d9bd50f6ea31a9fe12511b790fb469
David Fields <davidfields1027@gmail.com>
2025-06-14 08:03:36 -0400

    First attempt creating cfexp

--- cfe-constructors/cfexp-playground.rkt
+++ cfe-constructors/cfexp-playground.rkt
@@ -3,35 +3,33 @@
 (require  "../fsm-core/private/constants.rkt"
           "context-free-expressions-constructors.rkt")
 
-(define A (singleton-cfexp "a"))
-(define B (singleton-cfexp "b"))
-(define emp (empty-cfexp))
+(define E (empty-cfexp))
 
-(define ASB (add2env (add2env (concat-cfexp A B) (list "S" "aSb")) (list "S" EMP)))
+(define A (singleton-cfexp 'a))
 
-(define S (add2env (add2env (singleton-cfexp "aSb") (list "S" "aSb")) (list "S" (symbol->string EMP))))
+(define B (singleton-cfexp 'b))
 
-(define anbn (kleene-cfexp S))
-
-(define a^nb^n (kleene-cfexp (add2env (add2env (singleton-cfexp "S") (list "S" "aSb")) (list "S" (symbol->string EMP)))))
-
-(define anbn (concat-cfg (list A S B) (add2env 'S '((concat-cfg (list A S B)) emp))) '()))
-
-(define anbn-pre-expression (concat-pre-cfg (list A S B)))
-
-(define anbn (concat-cfg anbn-pre-expression (add2env 'S '(anbn-pre-expression emp))))
-
-;(define anbn (union-cfexp (concat-cfexp (list A S B) ) EMP))
-
-(define E (empty-cfexp (empty-cfexp-env)))
-
-(define A (singleton-cfexp 'a (empty-cfexp-env)))
-
-(define B (singleton-cfexp 'b (empty-cfexp-env)))
-
-(define ANBN (begin
+(define OLD-ANBN (let [(S-ENV void)
+                   (S void)]
+               (begin
                (set! S-ENV (empty-cfexp-env))
-               (set! S null)
-               (set! S-ENV (env-cfexp 'S (union-cfexp E (concat-cfexp (list A S B) S-ENV) S-ENV)))
-               (set! S (var-cfexp 'S (env-cfexp 'S S-ENV)))
-               S))
+               (set! S (var-cfexp 'S S-ENV))
+               (set! S-ENV (env-cfexp 'S (union-cfexp E (concat-cfexp (list A S B)))))
+               (set-var-cfexp-env! S S-ENV)
+               (set! S (var-cfexp 'S S-ENV))
+               S)))
+
+(define S (var-cfexp 'S (empty-cfexp-env)))
+
+(define ASB (concat-cfexp (list A S B)))
+
+(define EUASB (union-cfexp E (concat-cfexp (list A S B))))
+    
+(define ANBN (make-varcfexp-binding S EUASB))
+
+(define (loopinator cfe a-num)
+  (define (loopinator-helper a-num)
+    (if (= a-num 0)
+        '()
+        (cons (gen-cfexp-word cfe) (loopinator-helper (sub1 a-num)))))
+  (remove-duplicates (loopinator-helper a-num)))
--- cfe-constructors/context-free-expressions-constructors.rkt
+++ cfe-constructors/context-free-expressions-constructors.rkt
@@ -10,175 +10,166 @@
          concat-cfexp
          union-cfexp
          kleene-cfexp
-         add2env
-         gen-cfexp-word)
+         make-varcfexp-binding
+         gen-cfexp-word
+         empty-cfexp-env
+         (struct-out var-cfexp) ;;change to remove struct out
+         env-cfexp)
 
 ;;a context-free expression is either:
 ;; 1. null (base case)
 ;; 2. empty (base case)
 ;; 3. singleton (base case)
-;; 4. add2env (base case)
+;; 4. variable
 ;; 5. concat 
 ;; 6. union
 ;; 7. kleene
 
+
+;;hash ;;Purpose: To represent an empty environment
+(define (empty-cfexp-env) (hash))
+
+;;null-cfexp ;;Purpose: Only contains an environment
 (struct mk-null-cfexp (env) #:transparent)
 
+;;empty-cfexp ;;Purpose: A cfexp to represent the empty word
 (struct mk-empty-cfexp (env) #:transparent)
 
-(struct mk-singleton-cfexp (a env) #:transparent)
+;;singleton-cfexp ;;Purpose: A cfexp to represnt a single word
+(struct mk-singleton-cfexp (char env) #:transparent)
 
-(struct mk-concat-cfexp (cfe1 cfe2 env) #:transparent)
+;;concat-cfexp ;;Purpose: A cfexp to represent the concatentation of cfexps
+(struct mk-concat-cfexp (locfe env) #:transparent)
 
-(struct mk-union-cfexp (cfe1 cfe2 env) #:transparent)
+;;union-cfexp ;;Purpose: A cfexp to represent the union of cfexps
+(struct mk-union-cfexp (locfe env) #:transparent)
 
+;;Kleene-cfexp ;;Purpose: A cfexp to represent the Kleen of a cfexp
 (struct mk-kleene-cfexp (cfe env) #:transparent)
 
+;;variable-cfexp ;;Purpose: A cfexp to represent a variable that needs to be substituted
+(struct var-cfexp (cfe [env #:mutable]) #:transparent)
+
+;; -> null-cfexp
+;;Purpose: A wrapper to create a null-cfexp
 (define (null-cfexp)
-  (mk-null-cfexp (hash)))
+  (mk-null-cfexp (empty-cfexp-env)))
 
+;; -> empty-cfexp
+;;Purpose: A wrapper to create a empty-cfexp
 (define (empty-cfexp)
-  (mk-empty-cfexp (hash)))
+  (mk-empty-cfexp (empty-cfexp-env)))
 
+;; symbol -> singleton-cfexp
+;;Purpose: A wrapper to create a singleton-cfexp
 (define (singleton-cfexp a-char)
-  (mk-singleton-cfexp a-char (hash)))
+  (mk-singleton-cfexp a-char (empty-cfexp-env)))
 
-(define (concat-cfexp cfe1 cfe2)
-  (let ([cfe1-env (cond [(mk-null-cfexp? cfe1) (mk-null-cfexp-env cfe1)]
-                        [(mk-empty-cfexp? cfe1) (mk-empty-cfexp-env cfe1)]
-                        [(mk-singleton-cfexp? cfe1) (mk-singleton-cfexp-env cfe1)]
-                        [(mk-concat-cfexp? cfe1) (mk-concat-cfexp-env cfe1)]
-                        [(mk-union-cfexp? cfe1) (mk-union-cfexp-env cfe1)]
-                        [else (mk-kleene-cfexp-env cfe1)])]
-        [cfe2-env (cond [(mk-null-cfexp? cfe2) (mk-null-cfexp-env cfe2)]
-                        [(mk-empty-cfexp? cfe2) (mk-empty-cfexp-env cfe2)]
-                        [(mk-singleton-cfexp? cfe2) (mk-singleton-cfexp-env cfe2)]
-                        [(mk-concat-cfexp? cfe2) (mk-concat-cfexp-env cfe2)]
-                        [(mk-union-cfexp? cfe2) (mk-union-cfexp-env cfe2)]
-                        [else (mk-kleene-cfexp-env cfe2)])])
-    (mk-concat-cfexp cfe1 cfe2 (hash-union cfe1-env cfe2-env))))
+;;(listof cfexp) -> env
+;;Purpose: Merges all of the environments from the given (listof cfexp) in to one environment
+(define (merge-env locfexp)
+  (foldl (λ (cfe env)
+           (hash-union env
+                       (cond [(mk-null-cfexp? cfe) (mk-null-cfexp-env cfe)]
+                             [(mk-empty-cfexp? cfe) (mk-empty-cfexp-env cfe)]
+                             [(mk-singleton-cfexp? cfe) (mk-singleton-cfexp-env cfe)]
+                             [(var-cfexp? cfe) (var-cfexp-env cfe)]
+                             [(mk-concat-cfexp? cfe) (mk-concat-cfexp-env cfe)]
+                             [(mk-union-cfexp? cfe) (mk-union-cfexp-env cfe)]
+                             [else (mk-kleene-cfexp-env cfe)])))
+         (hash)
+         locfexp))
 
-(define (union-cfexp cfe1 cfe2)
-  (let ([cfe1-env (cond [(mk-null-cfexp? cfe1) (mk-null-cfexp-env cfe1)]
-                        [(mk-empty-cfexp? cfe1) (mk-empty-cfexp-env cfe1)]
-                        [(mk-singleton-cfexp? cfe1) (mk-singleton-cfexp-env cfe1)]
-                        [(mk-concat-cfexp? cfe1) (mk-concat-cfexp-env cfe1)]
-                        [(mk-union-cfexp? cfe1) (mk-union-cfexp-env cfe1)]
-                        [else (mk-kleene-cfexp-env cfe1)])]
-       [cfe2-env (cond [(mk-null-cfexp? cfe2) (mk-null-cfexp-env cfe2)]
-                        [(mk-empty-cfexp? cfe2) (mk-empty-cfexp-env cfe2)]
-                        [(mk-singleton-cfexp? cfe2) (mk-singleton-cfexp-env cfe2)]
-                        [(mk-concat-cfexp? cfe2) (mk-concat-cfexp-env cfe2)]
-                        [(mk-union-cfexp? cfe2) (mk-union-cfexp-env cfe2)]
-                        [else (mk-kleene-cfexp-env cfe2)])])
-    (mk-union-cfexp cfe1 cfe2 (hash-union cfe1-env cfe2-env))))
+;; . cfexp -> concat-cfexp
+;;Purpose: A wrapper to create a concat-cfexp
+(define (concat-cfexp . cfexp)
+  (let ([locfexp (flatten cfexp)])
+  (mk-concat-cfexp locfexp (merge-env locfexp))))
 
+;; . cfexp -> union-cfexp
+;;Purpose: A wrapper to create a union-cfexp
+(define (union-cfexp . cfexp)
+  (let ([locfexp (flatten cfexp)])
+    (mk-union-cfexp locfexp (merge-env locfexp))))
+
+;;cfexp -> Kleene-cfexp
+;;Purpose: A wrapper to create a Kleene-cfexp
 (define (kleene-cfexp cfe)
   (let ([cfe-env (cond [(mk-null-cfexp? cfe) (mk-null-cfexp-env cfe)]
                        [(mk-empty-cfexp? cfe) (mk-empty-cfexp-env cfe)]
                        [(mk-singleton-cfexp? cfe) (mk-singleton-cfexp-env cfe)]
                        [(mk-concat-cfexp? cfe) (mk-concat-cfexp-env cfe)]
                        [(mk-union-cfexp? cfe) (mk-union-cfexp-env cfe)]
+                       [(var-cfexp? cfe) (var-cfexp-env cfe)]
                        [else (mk-kleene-cfexp-env cfe)])])
     (mk-kleene-cfexp cfe cfe-env)))
 
-(define (add2env cfe language-map)
-  (define (add-to-env-helper hash category langauge)
-  (if (hash-has-key? hash category)
-      (hash-set hash category (cons langauge (hash-ref hash category)))
-      (hash-set hash category (list langauge))))
-  (cond [(mk-null-cfexp? cfe)
-         (struct-copy mk-null-cfexp cfe
-                 [env (add-to-env-helper (mk-null-cfexp-env cfe) (first language-map) (second language-map))])]
-        [(mk-empty-cfexp? cfe)
-         (struct-copy mk-empty-cfexp cfe
-                 [env (add-to-env-helper (mk-empty-cfexp-env cfe) (first language-map) (second language-map))])]
-        [(mk-singleton-cfexp? cfe)
-         (struct-copy mk-singleton-cfexp cfe
-                 [env (add-to-env-helper (mk-singleton-cfexp-env cfe) (first language-map) (second language-map))])]
-        [(mk-concat-cfexp? cfe)
-         (struct-copy mk-concat-cfexp cfe
-                 [env (add-to-env-helper (mk-concat-cfexp-env cfe) (first language-map) (second language-map))])]
-        [(mk-union-cfexp? cfe)
-         (struct-copy mk-union-cfexp cfe
-                 [env (add-to-env-helper (mk-union-cfexp-env cfe) (first language-map) (second language-map))])]
-        [else (struct-copy mk-kleene-cfexp cfe
-                 [env (add-to-env-helper (mk-kleene-cfexp-env cfe) (first language-map) (second language-map))])]))
+;;cfe-id cfe -> env
+;;Purpose: Creates an environment where the given cfe-id is the key and cfe is the value
+(define (env-cfexp cfe-id binding)
+  (hash cfe-id binding))
 
+;;var-cfexp cfe -> var-cfexp
+;;Purpose: Creates a binding where the cfe is bound to the given var-cfexp's environment
+(define (make-varcfexp-binding bindee binding)
+  (let ([env (var-cfexp-env bindee)]
+        [sym (var-cfexp-cfe bindee)])
+    (begin
+      (set! env (env-cfexp sym binding))
+      (set-var-cfexp-env! bindee env)
+      (set! bindee (var-cfexp sym env))
+      bindee)))
 
-  
+;;singleton-cfe -> word
+;;Purpose: Extracts the singleton 
+(define (convert-singleton cfe)
+  (list (mk-singleton-cfexp-char cfe))
+  ;;change to allow for numbers
+  #;(let ([element (mk-singleton-cfexp-a cfe)])
+    (if (not (string<=? "0" element "9"))
+        (list (string->symbol element))
+        (list (string->number element)))))
 
+;; union-cfexp --> cfexp
+;; Purpose: Return a randomly chosen sub-cfexp from the given union-cfexp
+(define (pick-cfexp cfexp)
+  (let [(cfexps (mk-union-cfexp-locfe cfexp))]
+    (list-ref cfexps (random (length cfexps)))))
 
-(define (gen-cfexp-word cfe . reps)
-  (define MAX-KLEENESTAR-REPS (if (empty? reps) 20 (first reps)))
-  ;; union-regexp --> regexp
-  ;; Purpose: Return a randomly chosen sub-regexp from the given union-regexp
-  (define (pick-cfexp e)
-    ;; union-rexp --> (listof regexp)
-    ;; Purpose: Extract the sub-regexps in the chain for the given union-regexp
-    (define (extract-union-cfexps cfexp)
-      (let [(r1 (mk-union-cfexp-cfe1 cfexp))
-            (r2 (mk-union-cfexp-cfe2 cfexp))]
-        (if (not (mk-union-cfexp? r2))
-            (list r1 r2)
-            (cons r1 (extract-union-cfexps r2)))))
-    (let [(cfexps (extract-union-cfexps e))]
-      (list-ref cfexps (random (length cfexps)))))
-  ;; -> (listof )
-  ;;Purpose: Extracts the singleton 
-  (define (convert-singleton)
-    (define (substitute-symbol sym)
-      (let ([env (mk-singleton-cfexp-env cfe)])
-      sym))
-    (let ([element (substitute-symbol (mk-singleton-cfexp-a cfe))])
-      (if (not (string<=? "0" element "9"))
-          (list (string->symbol element))
-          (list (string->number element)))))
-  (define (gen-concat-word concat-rexp gen-function reps)
-    ;; concat-rexp --> (listof regexp)
-    ;; Purpose: Extract the sub-regexps in the chain for the given concat-regexp
-    (define (extract-concat-regexps crexp)
-      (let [(r1 (mk-concat-cfexp-cfe1 crexp))
-            (r2 (mk-concat-cfexp-cfe2 crexp))]
-        (if (not (mk-concat-cfexp? r2))
-            (list r1 r2)
-            (cons r1 (extract-concat-regexps r2)))))
-    (let [(res (filter (λ (w) (not (eq? w EMP)))
-                       (flatten (map (λ (re) (gen-function re reps))
-                                     (extract-concat-regexps concat-rexp)))))]
+;;var-cfexp --> word
+;;Purpose: Substitutes the given var-cfexp with it's environment bindings 
+(define (substitute-var cfe)
+  (gen-cfexp-word (hash-ref (var-cfexp-env cfe) (var-cfexp-cfe cfe))))
+
+;;concat-cfexp --> word
+;;Purpose: Returns the concatenation of the sub context-free expressions 
+(define (gen-concat-word concat-cfexp gen-function reps)
+  (let [(res (filter (λ (w) (not (eq? w EMP)))
+                     (flatten (map (λ (cfe) (gen-function cfe reps))
+                                   (mk-concat-cfexp-locfe concat-cfexp)))))]
       (if (empty? res) EMP res)))
 
-  ;; natnum kleene-star-regexp (regexp --> word) --> word
-  ;; Purpose: Generate a word of arbitrary length in [0..reps+1] using
-  ;;          given regular expression and the given word-generating function
-  (define (gen-cfe-kleene-word cfe reps gen-function)
-    (let [(lst-words (filter
-                      (λ (w) (not (eq? w EMP)))
-                      (flatten
-                       (build-list
-                        (random (add1 reps))
-                        (λ (i) (gen-function (mk-kleene-cfexp-cfe cfe) reps))))))]
-      (if (empty? lst-words) EMP lst-words)))
-  
- 
+;; natnum kleene-star-cfexp (cfexp --> word) --> word
+;; Purpose: Generate a word of arbitrary length in [0..reps+1] using
+;;          given context-free expression and the given word-generating function
+(define (gen-cfe-kleene-word cfe reps gen-function)
+  (let [(lst-words (filter
+                    (λ (w) (not (eq? w EMP)))
+                    (flatten
+                     (build-list
+                      (random (add1 reps))
+                      (λ (i) (gen-function (mk-kleene-cfexp-cfe cfe) reps))))))]
+    (if (empty? lst-words) EMP lst-words)))
+
+
+;; cfe [natnum] -> word
+;; Purpose: Generates a word using 
+(define (gen-cfexp-word cfe . reps)
+  (define MAX-KLEENESTAR-REPS (if (empty? reps) 20 (first reps)))
   (cond [(mk-null-cfexp? cfe) (error "A word cannot be generated using the null-regexp.")]
         [(mk-empty-cfexp? cfe) EMP]
-        [(mk-singleton-cfexp? cfe) (convert-singleton)]
+        [(mk-singleton-cfexp? cfe) (convert-singleton cfe)]
+        [(var-cfexp? cfe) (substitute-var cfe)]
         [(mk-concat-cfexp? cfe) (gen-concat-word cfe gen-cfexp-word MAX-KLEENESTAR-REPS)]
-        [(mk-union-cfexp? cfe) (gen-cfexp-word (pick-cfexp cfe) MAX-KLEENESTAR-REPS)]
+        [(mk-union-cfexp? cfe) (gen-cfexp-word (pick-cfexp cfe)  MAX-KLEENESTAR-REPS)]
         [else (gen-cfe-kleene-word cfe MAX-KLEENESTAR-REPS gen-cfexp-word)]))
-
-
-
-#|
-(define sample-hash (hash))
-
-(define sh1 (if (hash-has-key? sample-hash 'S)
-    (hash-set sample-hash 'S (cons 'aSb (hash-ref sample-hash 'S)))
-    (hash-set sample-hash 'S (list 'aSb))))
-
-(define (sample-add-to-env hash category langauge)
-  (if (hash-has-key? hash category)
-      (hash-set hash category (cons langauge (hash-ref hash category)))
-      (hash-set hash category (list langauge))))
-|#
