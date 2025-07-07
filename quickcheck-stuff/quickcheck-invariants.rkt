9cece09eb87e99f42bdb63be091edf9760d3cb6c
Sora Garced <maldona2@shu.edu>
2025-07-07 19:46:11 -0400

    fixes

--- quickcheck-stuff/quickcheck-invariants.rkt
+++ quickcheck-stuff/quickcheck-invariants.rkt
@@ -2,19 +2,24 @@
 (require rackcheck "reg-exp-function.rkt" "testing-file-for-sm-test-invs.rkt")
 
 (define (translate-regexp regexp)
-  (cond [(null-regexp? (gen:list (gen:symbol (gen:const (first (string->list "∅"))) #:max-length 1) #:max-length 1))]
-        [(singleton-regexp? regexp) (gen:list (gen:symbol (gen:const (first (string->list (symbol->string (singleton-regexp-a regexp))))) #:max-length 1) 1)]
-        [(concat-regexp? regexp) (gen:tuple (translate-regexp (concat-regexp-r1 regexp)) (translate-regexp (concat-regexp-r2 regexp)))]
-        [(union-regexp? regexp) (gen:choice (translate-regexp (union-regexp-r1 regexp)) (translate-regexp (union-regexp-r2 regexp)))]
-        [(kleenestar-regexp? regexp) (gen:bind gen:natural (λ (x) (gen:list (translate-regexp regexp) #:max-length x)))]
-        [(empty-regexp? regexp) (gen:const '())]))
-
+  (cond [(null-regexp? regexp)
+         (gen:list (gen:symbol (gen:const (first (string->list "∅"))) #:max-length 1) #:max-length 1)]
+        [(singleton-regexp? regexp)
+         (gen:list (gen:symbol (gen:const (first (string->list (singleton-regexp-a regexp)))) #:max-length 1) #:max-length 1)]
+        [(concat-regexp? regexp)
+         (gen:tuple (translate-regexp (concat-regexp-r1 regexp)) (translate-regexp (concat-regexp-r2 regexp)))]
+        [(union-regexp? regexp)
+         (gen:choice (translate-regexp (union-regexp-r1 regexp)) (translate-regexp (union-regexp-r2 regexp)))]
+        [(kleenestar-regexp? regexp)
+         (gen:bind (gen:integer-in 0 20) (λ (x) (gen:list (translate-regexp (kleenestar-regexp-r1 regexp)) #:max-length x)))]
+        [(empty-regexp? regexp)
+         (gen:const '())]))
 
 (define (testing-function regexp inv)
   (let [(machine (regexp->fsa regexp))]
-    (check-property (property ([translated-regexp (translate-regexp regexp)])
+    (check-property (property ([translated-regexp (gen:map (translate-regexp regexp) flatten)])
                               (let [(flattened-translated-regexp (flatten translated-regexp))]
-                                (check-equal? (inv flattened-translated-regexp) (sm-apply machine flattened-translated-regexp)))))))
+                                (check-equal? (inv flattened-translated-regexp) (eq? (sm-apply machine flattened-translated-regexp) 'accept)))))))
 
 (define (quickcheck-invs machine los&regexp loi)
   (define state2inv (make-hash))
