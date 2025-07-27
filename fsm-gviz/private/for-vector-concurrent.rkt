ebbc205cfd8878c218979afa5a0726ee6107e84b
Sora Garced <maldona2@shu.edu>
2025-07-27 11:50:44 -0400

    improved speed of graphic loading and compiling, added new formatting capapbilities

---
+++ fsm-gviz/private/for-vector-concurrent.rkt
@@ -0,0 +1,33 @@
+#lang racket/base
+(require (for-syntax racket/base
+                     syntax/parse
+                     syntax/for-body
+                     racket/syntax)
+         racket/promise
+         racket/vector)
+(provide for/vector/concurrent)
+(define-syntax (for/vector/concurrent stx)
+  (syntax-parse stx
+    [(_ #:length len (clauses ...) body ...)
+     (with-syntax
+         ([((pre-body ...)
+            (post-body ...))
+           (split-for-body stx #'(body ...))]
+          [idx-id (generate-temporary 'idx)])
+       #'(for/fold/derived stx
+           ([promises (make-vector len)]
+            #:result (vector-map! force promises))
+           (clauses ...
+            [idx-id (in-naturals)])
+           pre-body ...
+           (begin
+             (vector-set! promises
+                        idx-id
+                        (delay/thread
+                         (let () post-body ...)))
+             promises)))]))
+
+(for/vector/concurrent
+    #:length 3
+  ([sym (in-list '(a b c))])
+  sym)
