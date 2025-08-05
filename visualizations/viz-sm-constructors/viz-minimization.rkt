cfc6614227c62bc60c316b93d493c3a94981fb1d
David Fields <davidfields1027@gmail.com>
2025-08-05 15:38:29 -0400

    started prep for unique sigma coloring

--- visualizations/viz-sm-constructors/viz-minimization.rkt
+++ visualizations/viz-sm-constructors/viz-minimization.rkt
@@ -32,6 +32,8 @@
 
 (define FONT-SIZE 20)
 
+(define loValidsig (append '(a b c d e f g h i j k l m n o p q r s t u v w x y z) (range 10)))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;STRUCTS;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;phase | all of the phases needed for the visualization | (zipperof phase)  
