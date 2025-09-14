50fb1ffb80c2cae5379d496093e1f51fce1a1872
Sora Garced <maldona2@shu.edu>
2025-09-14 11:09:09 -0400

    wip

--- fsm-gviz/private/for-vector-concurrent.rkt
+++ fsm-gviz/private/for-vector-concurrent.rkt
@@ -6,6 +6,7 @@
          racket/promise
          racket/vector)
 (provide for/vector/concurrent)
+
 (define-syntax (for/vector/concurrent stx)
   (syntax-parse stx
     [(_ #:length len (clauses ...) body ...)
@@ -14,20 +15,34 @@
             (post-body ...))
            (split-for-body stx #'(body ...))]
           [idx-id (generate-temporary 'idx)])
-       #'(for/fold/derived stx
-           ([promises (make-vector len)]
-            #:result (vector-map! force promises))
-           (clauses ...
-            [idx-id (in-naturals)])
-           pre-body ...
-           (begin
+       #'(let ([promises (make-vector len)])
+           (for (clauses ...
+                 [idx-id (in-naturals)])
+             pre-body ...
              (vector-set! promises
-                        idx-id
-                        (delay/thread
-                         (let () post-body ...)))
-             promises)))]))
+                          idx-id
+                          (delay/thread
+                           (let () post-body ...))))
+           (vector-map! force promises)
+           promises))]))
 
-(for/vector/concurrent
-    #:length 3
-  ([sym (in-list '(a b c))])
-  sym)
+#;(define-syntax (for/vector/concurrent stx)
+    (syntax-parse stx
+      [(_ #:length len (clauses ...) body ...)
+       (with-syntax
+           ([((pre-body ...)
+              (post-body ...))
+             (split-for-body stx #'(body ...))]
+            [idx-id (generate-temporary 'idx)])
+         #'(for/fold/derived stx
+             ([promises (make-vector len)]
+              #:result (vector-map! force promises))
+             (clauses ...
+              [idx-id (in-naturals)])
+             pre-body ...
+             (begin
+               (vector-set! promises
+                            idx-id
+                            (delay/thread
+                             (let () post-body ...)))
+               promises)))]))
--- fsm-gviz/private/parallel.rkt
+++ fsm-gviz/private/parallel.rkt
@@ -12,6 +12,30 @@
 
 (provide graphs->bitmap-thunks)
 
+;; On Mac/Linux we can bypass having to look at the systems PATH by instead
+;; using the absolute path to the executable. For unknown reasons this does not
+;; work on Windows so we will still use the PATH to call the dot executable
+;; Additionally, we need to use a different shell command for windows systems in order to get a status code back 
+(define (make-process-windows a-file-path)
+  (process (format "~a -T~s ~s -o ~s & echo %errorlevel%"
+                       "dot"
+                       'png
+                       (string-append a-file-path ".dot")
+                       (string-append a-file-path ".png"))))
+
+(define ((make-process-unix dot-executable-path) a-file-path)
+  (process (format "~a -T~s ~s -o ~s; echo $?"
+                       (path->string dot-executable-path)
+                       'png
+                       (string-append a-file-path ".dot")
+                       (string-append a-file-path ".png"))))
+
+(define (get-shell-process-result-windows shell-process-output)
+  (sync (read-line-evt shell-process-output 'any)))
+
+(define (get-shell-process-result-unix shell-process-output)
+  (sync (read-line-evt shell-process-output)))
+
 ;; Listof graph -> Listof Thunk
 ;; Creates all the graph images needed in parallel, and returns a list of thunks that will load them from disk
 (define (graphs->bitmap-thunks graphs
@@ -20,118 +44,98 @@
                                #:graph-type [graph-type 'rg]
                                #:cpu-cores [cpu-cores (void)])
   (define dot-executable-path (find-dot))
-  (if (path? dot-executable-path)
-      (let ([system-os (system-type)]
-            [cpu-cores-avail (make-semaphore (if (void? cpu-cores)
-                                                 (quotient (processor-count) 2)
-                                                 cpu-cores))]
-            [graphs-len (length graphs)]
-            [dot-executable-path-string (path->string dot-executable-path)]
-            [SAVE-DIR (find-tmp-dir)])
+  (cond [(path? dot-executable-path)
+         (define cpu-cores-avail (make-semaphore (if (void? cpu-cores)
+                                                     (quotient (processor-count) 2)
+                                                     cpu-cores)))
+         
+         (define graphs-len (length graphs))
+         (define-values (get-shell-process-result make-process)
+           (if (eq? (system-type) 'windows)
+               (values get-shell-process-result-windows make-process-windows)
+               (values get-shell-process-result-unix (make-process-unix dot-executable-path))))
+         (define SAVE-DIR (find-tmp-dir))
+         
+         (define img-vec
+           (cond [(eq? 'rg graph-type)
+                  (for/vector/concurrent
+                      #:length graphs-len
+                    ([graph (in-list graphs)]
+                     [idx (in-naturals)])
+                    (if (list? graph)
+                        (for/list/concurrent ([inner-graph (in-list graph)]
+                                              [inner-idx (in-range (length graph))])
+                          (graph->dot inner-graph SAVE-DIR (format "dot~s_~s" idx inner-idx))
+                          (format "~adot~s_~s" SAVE-DIR idx inner-idx))
+                        (begin
+                          (graph->dot graph SAVE-DIR (format "dot~s" idx))
+                          (format "~adot~s" SAVE-DIR idx))))]
+                 [(eq? 'cfg graph-type)
+                  (for/vector/concurrent
+                      #:length graphs-len
+                    ([graph (in-list graphs)]
+                     [idx (in-naturals)]
+                     [a-rank-node-lst (in-list rank-node-lst)])
+                    (if (list? graph)
+                        (for/list/concurrent ([inner-graph (in-list graph)]
+                                              [inner-idx (in-naturals)])
+                          (cfg-graph->dot inner-graph a-rank-node-lst SAVE-DIR (format "dot~s_~s" idx inner-idx))
+                          (format "~adot~s_~s" SAVE-DIR idx inner-idx))
+                        (begin
+                          (cfg-graph->dot graph a-rank-node-lst SAVE-DIR (format "dot~s" idx))
+                          (format "~adot~s" SAVE-DIR idx))))]
+                 [(eq? 'csg graph-type)
+                  (for/vector/concurrent
+                      #:length graphs-len
+                    ([graph (in-list graphs)]
+                     [idx (in-naturals)]
+                     [a-rank-node-lst (in-list rank-node-lst)])
+                    (if (list? graph)
+                        (for/list/concurrent ([inner-graph (in-list graph)]
+                                              [inner-idx (in-naturals)])
+                          (special-graph->dot inner-graph a-rank-node-lst SAVE-DIR (format "dot~s_~s" idx inner-idx))
+                          (format "~adot~s_~s" SAVE-DIR idx inner-idx))
+                        (begin
+                          (special-graph->dot graph a-rank-node-lst SAVE-DIR (format "dot~s" idx))
+                          (format "~adot~s" SAVE-DIR idx))))]))
         
-        ;; On Mac/Linux we can bypass having to look at the systems PATH by instead
-        ;; using the absolute path to the executable. For unknown reasons this does not
-        ;; work on Windows so we will still use the PATH to call the dot executable
-        ;; Additionally, we need to use a different shell command for windows systems in order to get a status code back 
-        (define (make-process file-path)
-          (process (if (eq? system-os 'windows)
-                       (format "~a -T~s ~s -o ~s & echo %errorlevel%"
-                               "dot"
-                               'png
-                               (string-append file-path ".dot")
-                               (string-append file-path ".png"))
-                       (format "~a -T~s ~s -o ~s; echo $?"
-                               dot-executable-path-string
-                               'png
-                               (string-append file-path ".dot")
-                               (string-append file-path ".png")))))
-
-        (define img-vec
-          (cond [(eq? 'rg graph-type)
-                 (for/vector/concurrent
-                     #:length graphs-len
-                   ([graph (in-list graphs)]
-                    [idx (in-naturals)])
-                   (if (list? graph)
-                       (for/list/concurrent ([inner-idx (in-range (length graph))]
-                                             [inner-graph (in-list graph)])
-                         (graph->dot inner-graph SAVE-DIR (format "dot~s_~s" idx inner-idx))
-                         (format "~adot~s_~s" SAVE-DIR idx inner-idx))
-                       (begin
-                         (graph->dot graph SAVE-DIR (format "dot~s" idx))
-                         (format "~adot~s" SAVE-DIR idx))))]
-                [(eq? 'cfg graph-type)
-                 (for/vector/concurrent
-                     #:length graphs-len
-                   ([graph (in-list graphs)]
-                    [idx (in-naturals)]
-                    [a-rank-node-lst (in-list rank-node-lst)])
-                   (if (list? graph)
-                       (for/list/concurrent ([inner-idx (in-naturals)]
-                                             [inner-graph (in-list graph)])
-                         (cfg-graph->dot inner-graph a-rank-node-lst SAVE-DIR (format "dot~s_~s" idx inner-idx))
-                         (format "~adot~s_~s" SAVE-DIR idx inner-idx))
-                       (begin
-                         (cfg-graph->dot graph a-rank-node-lst SAVE-DIR (format "dot~s" idx))
-                         (format "~adot~s" SAVE-DIR idx))))]
-                [(eq? 'csg graph-type)
-                 (for/vector/concurrent
-                     #:length graphs-len
-                   ([graph (in-list graphs)]
-                    [idx (in-naturals)]
-                    [a-rank-node-lst (in-list rank-node-lst)])
-                   (if (list? graph)
-                       (for/list/concurrent ([inner-idx (in-naturals)]
-                                             [inner-graph (in-list graph)])
-                         (special-graph->dot inner-graph a-rank-node-lst SAVE-DIR (format "dot~s_~s" idx inner-idx))
-                         (format "~adot~s_~s" SAVE-DIR idx inner-idx))
-                       (begin
-                         (special-graph->dot graph a-rank-node-lst SAVE-DIR (format "dot~s" idx))
-                         (format "~adot~s" SAVE-DIR idx))))]))
+         (define graphviz-thread-group (make-thread-group))
+         
+         (define (make-thread a-file-path)
+           (delay/thread
+            #:group graphviz-thread-group
+            (semaphore-wait cpu-cores-avail)
+            (define shell-process (make-process))
+            (define result (get-shell-process-result (first shell-process)))
+            (close-input-port (first shell-process))
+            (close-output-port (second shell-process))
+            (close-input-port (fourth shell-process))
+            (when (not (string=? "0" result))
+              (error (format "Graphviz produced an error while compiling the graphs: ~a" result)))
+            (semaphore-post cpu-cores-avail)
+            (string->path (format "~a.png" a-file-path))))
         
-        (define graphviz-thread-group (make-thread-group))
+         (define (compile-single-graph a-file-path graphic-formatter idx)
+           (define compiled-img-path
+             (make-thread a-file-path))
+           (vector-set! img-vec idx (lambda () (graphic-formatter (bitmap/file (force compiled-img-path))))))
 
-        (define (make-thread a-file-path)
-          (delay/thread
-           #:group graphviz-thread-group
-           (semaphore-wait cpu-cores-avail)
-           (let* ([shell-process (make-process a-file-path)]
-                  [result (sync (if (eq? system-os 'windows)
-                                    (read-line-evt (first shell-process) 'any)
-                                    (read-line-evt (first shell-process))))])
-               
-             (close-input-port (first shell-process))
-             (close-output-port (second shell-process))
-             (close-input-port (fourth shell-process))
-             (when (not (string=? "0" result))
-               (error (format "Graphviz produced an error while compiling the graphs: ~a" result)))
-             (semaphore-post cpu-cores-avail)
-             ;; TODO figure out how to get multiple graphs into singular thunk here
-             (string->path (format "~a.png" a-file-path)))))
-        
-        (define (compile-single-graph a-file-path graphic-formatter idx)
-          (define compiled-img-path
-            (make-thread a-file-path))
-          (vector-set! img-vec idx (lambda () (graphic-formatter (bitmap/file (force compiled-img-path))))))
-
-        (define (compile-multiple-graphs a-lst-of-file-paths graphic-formatter idx)
-          (define compiled-img-paths
-            (map (lambda (a-file-path)
-                   (make-thread a-file-path))
-                 a-lst-of-file-paths))
-          (vector-set! img-vec idx
-                       (lambda () (apply graphic-formatter
-                                   (map (lambda (img-path)  
-                                     (bitmap/file (force img-path))) compiled-img-paths)))))
-        (for ([idx (in-range (vector-length img-vec))])
-          (if (list? (vector-ref img-vec idx))
-              (compile-multiple-graphs (vector-ref img-vec idx)
-                                       (vector-ref graphic-formatter-vec idx)
-                                       idx)
-              (begin
-                (displayln "wtf")
-                (compile-single-graph (vector-ref img-vec idx)
-                                    (vector-ref graphic-formatter-vec idx)
-                                    idx))))
-        img-vec)
-      (error "Error caused when creating png file. This was probably due to the dot environment variable not existing on the path")))
+         (define (compile-multiple-graphs a-lst-of-file-paths graphic-formatter idx)
+           (define compiled-img-paths
+             (map (lambda (a-file-path)
+                    (make-thread a-file-path))
+                  a-lst-of-file-paths))
+           (vector-set! img-vec idx
+                        (lambda () (apply graphic-formatter
+                                          (map (lambda (img-path)  
+                                                 (bitmap/file (force img-path))) compiled-img-paths)))))
+         (for ([idx (in-range (vector-length img-vec))])
+           (if (list? (vector-ref img-vec idx))
+               (compile-multiple-graphs (vector-ref img-vec idx)
+                                        (vector-ref graphic-formatter-vec idx)
+                                        idx)
+               (compile-single-graph (vector-ref img-vec idx)
+                                     (vector-ref graphic-formatter-vec idx)
+                                     idx)))
+         img-vec]
+        [else (error "Error caused when creating png file. This was probably due to the dot environment variable not existing on the path")]))
