505ec3c3bc7662015f30830f1e4ba4aa8e1460f1
sophiaturano <sophiaturano81@gmail.com>
2025-10-15 11:16:26 -0400

    demo file

--- quickcheck-stuff/demo-file-sm-test-invs1.rkt
+++ quickcheck-stuff/demo-file-sm-test-invs1.rkt
@@ -20,12 +20,12 @@
 ;; C: (a*Ub*)c has been detected, final state
 
 (define EX-NDFA (make-ndfa '(S A B C)
-                             '(a b c)
-                             'S
-                             '(C)
-                             `((S ,EMP A) (S ,EMP B)
-                               (A a A) (A c C)
-                               (B b B) (B c C))))
+                           '(a b c)
+                           'S
+                           '(C)
+                           `((S ,EMP A) (S ,EMP B)
+                                        (A a A) (A c C)
+                                        (B b B) (B c C))))
 
 ;; word -> Boolean
 ;; Purpose: Determine if ci should be in S
@@ -44,12 +44,8 @@
   (andmap (λ (x) (eq? 'a x)) ci))
 
 ;; tests for A-INV-EX-NDFA
-(check-equal? (A-INV-EX-NDFA '()) #t)
-(check-equal? (A-INV-EX-NDFA '(a)) #t)
-(check-equal? (A-INV-EX-NDFA '(a a a a a)) #t)
-(check-equal? (A-INV-EX-NDFA '(a a b a c)) #f)
-(check-equal? (A-INV-EX-NDFA '(a c b)) #f)
-(check-equal? (A-INV-EX-NDFA '(a c a b c)) #f)
+;(check-inv-holds? A-INV-EX-NDFA '() '(a) '(a a a a a))
+;(check-inv-fails? A-INV-EX-NDFA '(a a b a c) '(a c b) '(a c a b c))
 
 ;; word -> Boolean
 ;; Purpose: Determine if ci should be in B
@@ -90,40 +86,40 @@
 
 
 
- ;; word → Boolean
- ;; Purpose: Determine if word contains aabab
- (define (contains-aabab? w)
- (and (>= (length w) 5)
- (or (equal? (take w 5) '(a a b a b))
- (contains-aabab? (rest w)))))
- ;; word word → Boolean
- ;; Purpose: Determine if second word ends with first word
- (define (end-with? suffix w)
- (and
- (>= (length w) (length suffix))
- (equal? suffix (take-right w (length suffix)))))
+;; word → Boolean
+;; Purpose: Determine if word contains aabab
+(define (contains-aabab? w)
+  (and (>= (length w) 5)
+       (or (equal? (take w 5) '(a a b a b))
+           (contains-aabab? (rest w)))))
+;; word word → Boolean
+;; Purpose: Determine if second word ends with first word
+(define (end-with? suffix w)
+  (and
+   (>= (length w) (length suffix))
+   (equal? suffix (take-right w (length suffix)))))
 
 
 
 ;; Let Σ = {a b}
 ;; L = {w | w does contains aabab 
 
-;; States
-;; S: b*, ab is detected, aabb is detected, or empty
-;; A: last of the ci is a and no aabab is detected
-;; B: last of ci is aa and no aabab is detected
-;; C: last of ci is aab and no aabab is detected
-;; D: last of ci is aaba and no aabab is detected
-;; E: aabab has been detected, final state
+;; State Documentation
+;; S: none of the pattern detected, starting state
+;; A: a detected and no other prefix detected 
+;; B: aa detected and no other prefix detected 
+;; C: aab detected and no other prefix detected 
+;; D: aaba detected and no other prefix detected 
+;; E: pattern detected, final state
 
 (define CONTAINS-aabab (make-dfa '(S A B C D E)
-                                  '(a b)
-                                  'S
-                                  '(E)
-                                  `((S a A) (S b S) (A a B) (A b S)
-                                    (B a B) (B b C) (C a D) (C b S)
-                                    (D a A) (D b E) (E a E) (E b E))
-                                  'no-dead))
+                                 '(a b)
+                                 'S
+                                 '(E)
+                                 `((S a A) (S b S) (A a B) (A b S)
+                                           (B a B) (B b C) (C a D) (C b S)
+                                           (D a A) (D b E) (E a E) (E b E))
+                                 'no-dead))
 
 (check-accept? CONTAINS-aabab '(a a b a b) '(b b a a a b a b b))
 (check-reject? CONTAINS-aabab '() '(b b a a a b a))
@@ -132,12 +128,12 @@
 
 ;; word → Boolean
 ;; Purpose: Determine that none of aabab is detected
- (define (S-INV-CONTAINS-aabab ci)
- (and (not (end-with? '(a) ci))
- (not (end-with? '(a a) ci))
- (not (end-with? '(a a b) ci))
- (not (end-with? '(a a b a) ci))
- (not (contains-aabab? ci))))
+(define (S-INV-CONTAINS-aabab ci)
+  (and (not (end-with? '(a) ci))
+       (not (end-with? '(a a) ci))
+       (not (end-with? '(a a b) ci))
+       (not (end-with? '(a a b a) ci))
+       (not (contains-aabab? ci))))
 
 ;; tests for S-INV-CONTAINS-aabab
 (check-equal? (S-INV-CONTAINS-aabab '()) #t)
@@ -156,10 +152,10 @@
 ;; word -> Boolean
 ;; Purpose: Determine if ci should be in A
 (define (A-INV-CONTAINS-aabab ci)
- (and (end-with? '(a) ci)
- (not (end-with? '(a a) ci))
- (not (end-with? '(a a b a) ci))
- (not (contains-aabab? ci))))
+  (and (end-with? '(a) ci)
+       (not (end-with? '(a a) ci))
+       (not (end-with? '(a a b a) ci))
+       (not (contains-aabab? ci))))
 
 
 ;; tests for A-INV-CONTAINS-aabab
@@ -174,8 +170,8 @@
 ;; word -> Boolean
 ;; Purpose: Determine if ci should be in B
 (define (B-INV-CONTAINS-aabab ci)
- (and (end-with? '(a a) ci)
- (not (contains-aabab? ci))))
+  (and (end-with? '(a a) ci)
+       (not (contains-aabab? ci))))
 
 ;; tests for B-INV-CONTAINS-aabab
 (check-equal? (B-INV-CONTAINS-aabab '(a a)) #t)
@@ -188,8 +184,8 @@
 ;; word -> Boolean
 ;; Purpose: Determine if ci should be in C
 (define (C-INV-CONTAINS-aabab ci)
- (and (end-with? '(a a b) ci)
- (not (contains-aabab? ci))))
+  (and (end-with? '(a a b) ci)
+       (not (contains-aabab? ci))))
 ;; tests for C-INV-CONTAINS-aabab
 (check-equal? (C-INV-CONTAINS-aabab '(a a b)) #t)
 (check-equal? (C-INV-CONTAINS-aabab '(a b b b a a b)) #t)
@@ -202,9 +198,9 @@
 
 ;; word -> Boolean
 ;; Purpose: Determine if ci should be in D
- (define (D-INV-CONTAINS-aabab ci)
- (and (end-with? '(a a b a) ci)
- (not (contains-aabab? ci))))
+(define (D-INV-CONTAINS-aabab ci)
+  (and (end-with? '(a a b a) ci)
+       (not (contains-aabab? ci))))
     
 ;; tests for D-INV-CONTAINS-aabab
 (check-equal? (D-INV-CONTAINS-aabab '(a a b a)) #t)
@@ -216,7 +212,7 @@
 
 ;; word -> Boolean
 ;; Purpose: Determine if ci should be in E
- (define E-INV-CONTAINS-aabab contains-aabab?)
+(define E-INV-CONTAINS-aabab contains-aabab?)
 
 
 ;; tests for E-INV-CONTAINS-aabab
@@ -228,5 +224,16 @@
 (check-equal? (E-INV-CONTAINS-aabab '(a b b b a a b a a)) #f)
 
 
+#;(sm-test-invs CONTAINS-aabab
+              (list 'S S-INV-CONTAINS-aabab)
+              (list 'A A-INV-CONTAINS-aabab)
+              (list 'B B-INV-CONTAINS-aabab)
+              (list 'C C-INV-CONTAINS-aabab)
+              (list 'D D-INV-CONTAINS-aabab)
+              (list 'E E-INV-CONTAINS-aabab))
+
+#;(sm-all-possible-words CONTAINS-aabab)
+#;(sm-viz CONTAINS-aabab '(a a b a a))
+#;(sm-graph CONTAINS-aabab)
 
 
