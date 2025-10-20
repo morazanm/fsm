83ea9779fb35764ae23d2f2e5d7a8040075e1d7e
Andres Garced <andresgarced22@proton.me>
2025-10-20 18:42:34 -0400

    last fix for invs

--- visualizations/viz-grammar-constructors/rg-viz.rkt
+++ visualizations/viz-grammar-constructors/rg-viz.rkt
@@ -51,12 +51,12 @@
 
 ;; levels -> tree
 ;; Creates a tree structure from the levels
-(define (create-yield-tree levels)
+(define (create-yield-tree levels start-elem)
   (foldl (lambda (val accum)
            (set-tree-subtrees! (dfs accum (first (first val)))
                                (map (lambda (edge) (tree (second edge) '())) val))
            accum)
-         (tree 'S '())
+         (tree start-elem '())
          levels))
 
 ;; Symbol -> Symbol
@@ -379,6 +379,13 @@
          [producing-nodes (get-producing-nodes (append* levels))]
          [invariant-nodes
           (cons root-node
+                (append-map
+                 (lambda (lvl)
+                   (filter (lambda (node) (member node producing-nodes))
+                                               (map (lambda (edge) (second edge))
+                                                    (filter (lambda (edge) (not (empty? edge))) lvl))))
+                 levels))
+          #;(cons root-node
                 (append-map (lambda (lvl)
                               (let* ([nodes (map (lambda (edge) (second edge))
                                                  (filter (lambda (edge) (not (empty? edge))) lvl))])
@@ -438,6 +445,10 @@
                                            (symbol->string (third (second derv-elem))))))]
              [used-node-names (make-hash)]
              [renamed (rename-nodes (rename-edges (create-edges w-der) used-node-names) used-node-names)]
+             [yield-trees (map (lambda (x) (create-yield-tree x (grammar-start rg))) (map reverse (create-list-of-levels (map (λ (el) (if (symbol? (first el))
+                                                                                (list el)
+                                                                                el))
+                                                                    renamed))))]
              [dgraph (dgrph (map (λ (el) (if (symbol? (first el))
                                              (list el '())
                                              el))
@@ -447,12 +458,15 @@
                             '()
                             (rest rules)
                             (list (first rules))
-                            (reverse (for/list ([lvl (in-list (create-list-of-levels
+                            
+                            (map (lambda (x) (first yield-trees))
+                                 yield-trees)
+                            #;(reverse (for/list ([lvl (in-list (create-list-of-levels
                                                                (map (λ (el) (if (symbol? (first el))
                                                                                 (list el)
                                                                                 el))
                                                                     renamed)))])
-                                       (create-yield-tree (reverse lvl))))
+                                       (create-yield-tree (reverse lvl) (grammar-start rg))))
                             (list (tree (grammar-start rg) '())))]
              [lod (reverse (create-dgrphs dgraph '()))]
              [broken-invariants
@@ -483,3 +497,37 @@
                            (C ,ARROW bA)
                            (C ,ARROW b))
            'S))
+
+
+
+
+
+
+
+(define MULT3-as 
+  (make-unchecked-rg '(S B C)
+           '(a b)
+           `((S -> ,EMP) (S -> aB) (S -> bB)  
+             (B -> aC)  (B -> bB)
+             (C -> aS)  (C -> bS))
+           'S))
+
+(define (S-INV w)
+  (= 0 (remainder (length (filter (lambda (s) (eq? s' a)) w)) 3)))
+
+(define (B-INV w)
+  (let [(as (filter (lambda (s) (eq? s 'a)) w))]
+    (and (>= (length as) 2)
+         (= 2 (remainder (length as) 3)))))
+
+(define (C-INV w)
+  (let [(as (filter (lambda (s) (eq? s 'a)) w))]
+    (and (>= (length as) 1)
+         (= 1 (remainder (length as) 3)))))
+
+(rg-viz
+   MULT3-as
+   '(b a a)
+   (list 'S S-INV)
+   (list 'B B-INV)
+   (list 'C C-INV))
